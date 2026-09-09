58d815cf21586d7f0573345607755342456bc4e8
ccabatay10-PrintWise <ccabatay10@gmail.com>
2026-09-09 22:00:51 +0800

    Mount receipt printing animation

--- app/pos/layout.tsx
+++ app/pos/layout.tsx
@@ -4,8 +4,9 @@ import DiscountQuickModal from "./DiscountQuickModal";
 import OrdersQuickModal from "./OrdersQuickModal";
 import PrinterQuickModal from "./PrinterQuickModal";
 import SettingsQuickModal from "./SettingsQuickModal";
+import PrintReceiptAnimation from "./PrintReceiptAnimation";
 import "./sale-complete.css";
 
 export default function POSLayout({ children }: { children: React.ReactNode }) {
-  return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal /><SettingsQuickModal />{children}</>;
+  return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal /><SettingsQuickModal /><PrintReceiptAnimation />{children}</>;
 }
