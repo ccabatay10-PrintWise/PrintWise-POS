225cd8ad7c6520af04562da7969a378de9b4ff27
ccabatay10-PrintWise <ccabatay10@gmail.com>
2026-09-09 21:22:24 +0800

    Mount POS printer settings modal

--- app/pos/layout.tsx
+++ app/pos/layout.tsx
@@ -2,7 +2,8 @@
 
 import DiscountQuickModal from "./DiscountQuickModal";
 import OrdersQuickModal from "./OrdersQuickModal";
+import PrinterQuickModal from "./PrinterQuickModal";
 
 export default function POSLayout({ children }: { children: React.ReactNode }) {
-  return <><DiscountQuickModal /><OrdersQuickModal />{children}</>;
+  return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal />{children}</>;
 }
