3a6697eef6c93ee204eb56a684fd4c4b3069fae9
ccabatay10-PrintWise <ccabatay10@gmail.com>
2026-09-09 21:55:05 +0800

    Load complete-sale confirmation styles

--- app/pos/layout.tsx
+++ app/pos/layout.tsx
@@ -4,6 +4,7 @@ import DiscountQuickModal from "./DiscountQuickModal";
 import OrdersQuickModal from "./OrdersQuickModal";
 import PrinterQuickModal from "./PrinterQuickModal";
 import SettingsQuickModal from "./SettingsQuickModal";
+import "./sale-complete.css";
 
 export default function POSLayout({ children }: { children: React.ReactNode }) {
   return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal /><SettingsQuickModal />{children}</>;
