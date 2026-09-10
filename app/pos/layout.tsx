30d6164b286b1a62711b71571dc2b54b31a4ea4b
ccabatay10-PrintWise <ccabatay10@gmail.com>
2026-09-10 20:56:38 +0800

    Connect WISE MENU payments to POS

--- app/pos/layout.tsx
+++ app/pos/layout.tsx
@@ -5,8 +5,9 @@ import OrdersQuickModal from "./OrdersQuickModal";
 import PrinterQuickModal from "./PrinterQuickModal";
 import SettingsQuickModal from "./SettingsQuickModal";
 import PrintReceiptAnimation from "./PrintReceiptAnimation";
+import WiseMenuPaymentModal from "./WiseMenuPaymentModal";
 import "./sale-complete.css";
 
 export default function POSLayout({ children }: { children: React.ReactNode }) {
-  return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal /><SettingsQuickModal /><PrintReceiptAnimation />{children}</>;
+  return <><DiscountQuickModal /><OrdersQuickModal /><PrinterQuickModal /><SettingsQuickModal /><PrintReceiptAnimation /><WiseMenuPaymentModal />{children}</>;
 }
